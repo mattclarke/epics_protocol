f75951a8826293b497ef032c0f0d588c9641cb93
Matt Clarke <matt.clarke@ess.eu>
2023-11-04 17:06:34 +0100

    added timeout to udp recv

--- lib/epics_protocol.ex
+++ lib/epics_protocol.ex
@@ -13,9 +13,8 @@ defmodule EpicsProtocol do
     data = Epics.Protocol.create_search_message(port, name)
 
     # Need to auto configure ip address somehow
-    # Need timeouts
     :ok = :gen_udp.send(socket, {192, 168, 0, 255}, 5076, data)
 
-    :gen_udp.recv(socket, 0)
+    :gen_udp.recv(socket, 0, _timeout = 5000)
   end
 end
