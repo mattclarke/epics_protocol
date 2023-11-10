585e572f98a447053c62956b70964668e5334c55
Matt Clarke <matt.clarke@ess.eu>
2023-11-10 16:47:19 +0100

    starting to decode channelGetResponseInit

--- lib/epics/get_command.ex
+++ lib/epics/get_command.ex
@@ -1,4 +1,9 @@
 defmodule Epics.GetCommand do
+  alias Epics.GetCommand
+  defstruct [:flags, :request_id, :status, :introspection_id]
+
+  @init_cmd 0x08
+
   def create_init_get_command(server_channel_id, request_id) do
     [
       # HEADER
@@ -18,7 +23,7 @@ defmodule Epics.GetCommand do
       # requestID
       <<request_id::32-little>>,
       # subcommand
-      0x08,
+      @init_cmd,
       # FieldDesc
       0xFD,
       # Size?
@@ -32,5 +37,41 @@ defmodule Epics.GetCommand do
       # ?
       0x00
     ]
+  end
+
+  def decode_channel_get_response_init(data) do
+    case data do
+      <<0xCA, _version, flags, 0x0A, <<_payload_size::32-little>>, payload::binary>> ->
+        << <<request_id::32-little>>, @init_cmd, status, rest::binary>> = payload
+        # TODO: create status module as this is duplicated code
+        status =
+          case status do
+            # Short-hand for OK and string fields omitted.
+            255 -> :ok
+            0 -> :ok
+            1 -> :warning
+            2 -> :error
+            _ -> :fatal
+          end
+
+        # TODO: handle case when status is not 255 and we need to extract the string
+        # TODO: don't decode the rest on error or fatal
+
+        # starts with fd then introspection ID (unique short)
+        <<0xFD, introspection_id::16-little, field_description, rest::binary>> = rest
+
+        case field_description do
+          128 ->
+            # Structure: identification string + (field name, FieldDesc)[]
+            << string_length, rest::binary >> = rest
+            <<type_string::binary-size(string_length), rest::binary>> = rest
+            IO.inspect(type_string)
+        end
+
+
+        {:ok, %GetCommand{flags: flags, request_id: request_id, status: status, introspection_id: introspection_id}}
+      _ -> {:error, "Binary data does not conform to expected channelGetResponseInit format"}
+    end
+
   end
 end
