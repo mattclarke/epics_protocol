e96be3f5625bc0b59ce566a6855623f569f07701
Matt Clarke <matt.clarke@ess.eu>
2023-11-14 06:24:25 +0100

    now creates a field description map

--- lib/epics/get_command.ex
+++ lib/epics/get_command.ex
@@ -1,6 +1,6 @@
 defmodule Epics.GetCommand do
   alias Epics.GetCommand
-  defstruct [:flags, :request_id, :status, :introspection_id, :type_string]
+  defstruct [:flags, :request_id, :status, :fields]
 
   @init_cmd 0x08
 
@@ -42,7 +42,7 @@ defmodule Epics.GetCommand do
   def decode_channel_get_response_init(data) do
     case data do
       <<0xCA, _version, flags, 0x0A, <<_payload_size::32-little>>, payload::binary>> ->
-        << <<request_id::32-little>>, @init_cmd, status, rest::binary>> = payload
+        <<(<<request_id::32-little>>), @init_cmd, status, rest::binary>> = payload
         # TODO: create status module as this is duplicated code
         status =
           case status do
@@ -60,48 +60,56 @@ defmodule Epics.GetCommand do
         # starts with fd then introspection ID (unique short)
         <<0xFD, introspection_id::16-little, field_description, _rest::binary>> = rest
 
-        case field_description do
-          128 ->
-            decode_structure(rest)
-        end
+        {fields, _} =
+          case field_description do
+            128 ->
+              decode_structure(rest)
+          end
 
+        {:ok, %GetCommand{flags: flags, request_id: request_id, status: status, fields: fields}}
 
-        {:ok, %GetCommand{flags: flags, request_id: request_id, status: status, introspection_id: introspection_id}}
-      _ -> {:error, "Binary data does not conform to expected channelGetResponseInit format"}
+      _ ->
+        {:error, "Binary data does not conform to expected channelGetResponseInit format"}
     end
-
   end
 
   defp decode_structure(data) do
     <<0xFD, introspection_id::16-little, 128, rest::binary>> = data
     # Structure: identification string + (field name, FieldDesc)[]
     <<string_length, structure_name::binary-size(string_length), rest::binary>> = rest
-    IO.inspect(structure_name)
+
+    structure_name =
+      case structure_name do
+        "" -> "structure"
+        _ -> structure_name
+      end
+
     # Next byte defines the number of upper level fields
     <<num_fields, rest::binary>> = rest
-    {_result, rest} = Enum.reduce(0..(num_fields - 1), {[], rest}, fn i, {acc, payload} ->
-      {acc, decode_name_and_type(payload)}
+    structure = %{"introspection_id" => introspection_id, "name" => structure_name}
+
+    Enum.reduce(0..(num_fields - 1), {structure, rest}, fn _i, {acc, payload} ->
+      {name, type, rest} = decode_name_and_type(payload)
+      acc = Map.put(acc, name, type)
+      {acc, rest}
     end)
-    rest
   end
 
   defp decode_name_and_type(data) do
     # Name of field then type
-    << string_length, name::binary-size(string_length), rest::binary>> = data
+    <<string_length, name::binary-size(string_length), rest::binary>> = data
     <<typecode, rest::binary>> = rest
-    IO.inspect(name)
-    IO.inspect(typecode)
-    {type, rest} =
-    case typecode do
-      0x60 -> {"string", rest}
-      0x22 -> {"int", rest}
-      0x23 -> {"long", rest}
-      0x43 -> {"double", rest}
-      0x68 -> {"string[]", rest}
-      0xFD -> {"structure", decode_structure(<<typecode, rest::binary>>)}
 
-    end
-    IO.inspect(type)
-    rest
+    {type, rest} =
+      case typecode do
+        0x60 -> {"string", rest}
+        0x22 -> {"int", rest}
+        0x23 -> {"long", rest}
+        0x43 -> {"double", rest}
+        0x68 -> {"string[]", rest}
+        0xFD -> decode_structure(<<typecode, rest::binary>>)
+      end
+
+    {name, type, rest}
   end
 end
