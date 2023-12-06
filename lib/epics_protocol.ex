99aab22f2773c3ade7f785ab8f0464799a68ba92
Matt Clarke <matt.clarke@ess.eu>
2023-12-06 13:25:35 +0900

    pvget works for basic pv types

--- lib/epics_protocol.ex
+++ lib/epics_protocol.ex
@@ -40,31 +40,46 @@ defmodule EpicsProtocol do
     {:ok, socket}
   end
 
-  def create_channel(socket, pvname) do
+  def create_channel(socket, pvname, request_id) do
     # TODO: these numbers (1234) need to be unique
-    request = Epics.Channel.create_channel_request(pvname, 1234)
+    request = Epics.Channel.create_channel_request(pvname, request_id)
     :ok = :gen_tcp.send(socket, request)
-
     {:ok, reply} = :gen_tcp.recv(socket, 0, 5000)
-    {:ok, response} = Epics.Channel.decode_create_channel_response(reply, 1234)
-    get_cmd = Epics.GetCommand.create_init_get_command(response.server_channel_id, 12345)
+    Epics.Channel.decode_create_channel_response(reply, request_id)
+  end
+
+  def get_command_init(socket, server_channel_id, request_id) do
+    # Do initial get to get the structure with values
+    get_cmd = Epics.GetCommand.create_init_get_command(server_channel_id, request_id)
     :ok = :gen_tcp.send(socket, get_cmd)
     {:ok, reply} = :gen_tcp.recv(socket, 0, 5000)
-
-    # Reply is channelGetResponseInit
     {:ok, structure} = Epics.GetCommand.decode_channel_get_response_init(reply)
+  end
 
-    # Make a proper get request
-    get_cmd = Epics.GetCommand.create_get_command(response.server_channel_id, 12345)
+  def get_command(socket, server_channel_id, request_id, fields) do
+    # Make a proper get request to get the values
+    get_cmd = Epics.GetCommand.create_get_command(server_channel_id, request_id)
     :ok = :gen_tcp.send(socket, get_cmd)
     {:ok, reply} = :gen_tcp.recv(socket, 0, 5000)
-    IO.inspect(reply, limit: :infinity)
-    {:ok, values} = Epics.GetCommand.decode_channel_get_response(structure.fields, reply)
+    {:ok, response} = Epics.GetCommand.decode_channel_get_response(fields, reply)
+  end
 
+  def print_pv_data(fields, values) do
+    value_paths = Epics.PvStructure.get_value_paths_in_order(fields)
+      value_paths
+        |> Enum.reduce(nil, fn path, acc ->
+          value = Map.get(values, path)
+          spath = Enum.join(path, ":")
+          IO.puts("#{spath} #{inspect value}")
+          acc
+      end)
   end
 
   def pvget(pvname, address, port) do
     {:ok, socket} = establish_connection(address, port)
-    create_channel(socket, pvname)
+    {:ok, response} = create_channel(socket, pvname, 12345)
+    {:ok, structure} = get_command_init(socket, response.server_channel_id, 12345)
+    {:ok, response} = get_command(socket, response.server_channel_id,12345, structure.fields)
+    print_pv_data(structure.fields, response.values)
   end
 end
